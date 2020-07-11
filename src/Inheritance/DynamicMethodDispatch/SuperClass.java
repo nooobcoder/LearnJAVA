293d0fb7d2b5ff98e4c8f9d174559fdf738707d2
nooobcoder <suryashi2013@gmail.com>
2020-07-11 14:35:48 +0530

    Dynamic Method Dispatch codes included

---
+++ src/Inheritance/DynamicMethodDispatch/SuperClass.java
@@ -0,0 +1,12 @@
+package Inheritance.DynamicMethodDispatch;
+
+// This is the parent class that would be inherited by the child classes
+
+public class SuperClass {
+    SuperClass() {
+    }
+
+    public void displayMessage() {
+        System.out.println("Inside Base class function");
+    }
+}
