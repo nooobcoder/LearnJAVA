f3b0c9f9295562ab22e2ce6c3714a5946f9630e8
nooobcoder <suryashi2013@gmail.com>
2021-01-15 01:14:18 +0530

    "Minor Changes"

---
+++ src/LambdaExpressions/MethodReferences/TestCodeForMethodReference/ArgumentTest.java
@@ -0,0 +1,9 @@
+package LambdaExpressions.MethodReferences.TestCodeForMethodReference;
+
+public class ArgumentTest {
+    public static void main(String[] args) {
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
---
+++ src/LambdaExpressions/MethodReferences/TestCodeForMethodReference/MethodReferenceExample2.java
@@ -0,0 +1,31 @@
+package LambdaExpressions.MethodReferences.TestCodeForMethodReference;
+
+// Creating a functional interface below
+interface ReverseString {
+    String reverser(String s);
+}
+
+class ReferencerClass {
+    static String getReversedString(String str) {
+        StringBuilder result = new StringBuilder();
+        for (int h = str.length() - 1; h >= 0; --h) {
+            result.append(str.charAt(h));
+        }
+        return result.toString();
+    }
+
+    static String interfaceMethodReferencer(ReverseString rs, String str) {
+        return rs.reverser(str);
+    }
+}
+
+public class MethodReferenceExample2 extends ReferencerClass {
+    public static void main(String[] args) {
+        String inStr = "Lambdas are the beauty of Java";
+        String outStr = ""; // This shall contain the reversed string
+
+        outStr = ReferencerClass.interfaceMethodReferencer(ReferencerClass::getReversedString, inStr);
+        System.out.println("Original String:  " + inStr);
+        System.out.println("Reversed String: " + outStr);
+    }
+}
